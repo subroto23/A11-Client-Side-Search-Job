8c62d0b40c81e986cef738e42913f2232f4a6937
Subroto Das <rupashi.bangla.club@gmail.com>
2023-11-18 16:24:19 +0600

    Animation NewsLetter

--- src/Components/SubscribeNews/SubscribeNewsletter.jsx
+++ src/Components/SubscribeNews/SubscribeNewsletter.jsx
@@ -1,7 +1,17 @@
+import styles from "./SubscribeNews.module.css";
+import { MdOutlineAlternateEmail } from "react-icons/md";
 const SubscribeNewsletter = () => {
   return (
     <div className="flex justify-center mt-6">
-      <div className=" bg-white md:my-6 w-11/12 border">
+      <div className=" bg-white md:my-6 w-11/12 border relative">
+        <div className={`text-2xl text-green-600 font-bold md:block hidden`}>
+          <span className={styles.animated1}>
+            <MdOutlineAlternateEmail />
+          </span>
+          <span className={styles.animated2}>
+            <MdOutlineAlternateEmail />
+          </span>
+        </div>
         <div className="flex justify-center py-6 flex-col items-center px-2 md:my-4">
           <h1 className="text-gray-500 md:text-4xl text-2xl font-semibold mt-6 md:mb-12 mb-6">
             Subscribe Newsletter
